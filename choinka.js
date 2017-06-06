92e4a56865253eae7a3327be248589c1cc53ad56
Your Name <you@example.com>
2017-06-06 11:14:40 +0200

    new

---
+++ choinka.js
@@ -0,0 +1,23 @@
+// pierwsza po mojemu
+
+for (i=0;i<=5;i++)
+{
+for(j=0; j<=i; j++)
+{
+document.write("*");
+}
+document.write("<br>");
+}
+
+// druga z "instrukcji"
+
+var drawTree(i) {
+for (i=1;i<=5;1++) {
+var star()
+}
+for (j=0;j<=1;j++) {
+var star(star += '*');
+}
+}
+console.log(star);
+
